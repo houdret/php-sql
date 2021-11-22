e58fd393e789d3aa211295c55bbf58229d76605d
houdret <jeanhoudret2006@hotmail.com>
2021-11-22 15:29:47 +0100

    checked vérification

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+error_reporting(E_ALL ^ E_WARNING);
+$bdd = new PDO('mysql:host=localhost;dbname=weatherapp', 'root', '');
+                
+$resultat = $bdd->query("SELECT * FROM météo");
+$donnees = $resultat->fetch();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Exercice sql Météo">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Météo</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Température pour chaque ville haute et basse !</h1>
+       
+        <table class="table table-striped">
+            <?php while ($donnees = $resultat->fetch())
+                {
+                    echo '<tr> <td>'.$donnees['ville'].'</td><td>'.$donnees['haut'].'</td><td>'.$donnees['bas'].'</td></tr>';
+                } ?>
+        </table>
+        <form action="" method="post" class=" was-validated row g-1 ">
+            <label for="ville" class="form-label">Ville </label> 
+            <input type="text" name="ville" class="form-control is-valid" required><br/>            
+            <label for="haut" class="form-label">Haut </label> 
+            <input type="text" name="haut" class="form-control is-valid" required><br/> 
+            <label for="bas" class="form-label">Bas </label> 
+            <input type="text" name="bas" class="form-control is-valid" required><br/>  
+            <div class="input-group mb-3">
+            <div class="input-group-text">
+                <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" name="checked">
+            </div>
+            <?php while ($donnees = $resultat->fetch())
+                {
+                    echo '<input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" name="checked">';
+                    echo '<input type="text" class="form-control" aria-label="Text input with checkbox" value="'.$donnees['ville'].'" aria-label="Checkbox for following text input">';
+
+                } ?>
+            </div>
+            <?php ?>
+            <div class="col-12">
+                <button type="submit" name="submit" class="btn btn-primary mb-3">Enregistrer</button>
+            </div>                      
+        </form>
+        <?php
+        if (isset($_POST['submit'])) {
+            $errors = array();      
+            $ville = filter_var($_POST['ville'], FILTER_SANITIZE_STRING);
+            $haut  = filter_var($_POST['haut'], FILTER_SANITIZE_STRING);
+            $bas = filter_var($_POST['bas'], FILTER_SANITIZE_STRING);
+            $checked = $_POST['checked'];
+            if (false === filter_var($ville, FILTER_SANITIZE_STRING)) {
+                $errors['ville'] = '<div class="alert alert-danger" role="alert">la ville est invalide.</div>';
+            } else {
+                echo '<div class="alert alert-success" role="alert">la ville est considérée comme valide.</div>';
+            }
+            if (false === filter_var($haut, FILTER_SANITIZE_STRING)) {
+                $errors['haut'] = '<div class="alert alert-danger" role="alert">la température haute est invalide.</div>';
+            } else {
+                echo '<div class="alert alert-success" role="alert">la température haute est bien nettoyé et est considérée comme valide.</div>';
+            }
+            if (false === filter_var($gender, FILTER_SANITIZE_STRING)) {
+                $errors['bas'] = '<div class="alert alert-danger" role="alert">la température basse  est invalide.</div>';
+            } else {
+                echo '<div class="alert alert-success" role="alert">la température basse est bien nettoyé et est considérée comme valide.</div>';                
+            }
+            $data = [
+                'ville' => $_POST['ville'],
+                'haut' => $_POST['haut'],
+                'bas' => ($_POST['bas']),                
+            ];
+            try
+            {
+                $insertion =
+                'INSERT INTO météo (ville, haut, bas ) VALUES (:ville,:haut,:bas)';
+                $bdd->prepare($insertion)->execute($data);
+
+                
+                if ($bdd == true) {
+                    echo '<div class="alert alert-success" role="alert">les données ont bien été enregistrées! </div>';
+                } 
+            }
+            catch(Exception $e)
+            {
+                die('Erreur : '.$e->getMessage());
+            }          
+        }
+        ?>
+        </div>
+    </body>
+</html>
+<?php
+
+$resultat->closeCursor();
+?>
